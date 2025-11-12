3c3782a3e193e09f0eb9e35d79aca3b03b815e97
Fu4di4chmd <achmadsaifulfuadifuad@gmail.com>
2025-11-12 22:53:55 +0700

    UPDATE : add fitures css in folder->dashboard/index.php

--- components/header.php
+++ components/header.php
@@ -1,3 +1,21 @@
 <?php
 // header.php
 ?>
+<link rel="stylesheet" href="../source/css/header.css">
+ <header>
+    <div class="container">
+        <h1 class="logo">NAMA PESANTREEEEEN</h1>
+        <nav>
+            <ul>
+                <li><a href="../dashboard/index.php">beranda</a></li>
+                <li><a href="../dashboard/daftar_siswa.php">Informasi</a></li>
+                <li><a href="../siswa/registrasi.php">Registrasi</a></li>
+                <li><a href="../auth/login_siswa.php">Login Siswa</a></li>
+                <li><a href="../siswa/edit_siswa.php"><img src="../upload/<?= $siswa['FOTO_SISWA_SISWA']; ?>" 
+                    alt="Foto Siswa" class="foto-profil">
+                    </a>
+                    <!-- <a href="../auth/logout.php">Logout</a></li> -->
+            </ul>
+        </nav>
+    </div>
+</header>
--- dashboard/index.php
+++ dashboard/index.php
@@ -4,6 +4,7 @@ if (!isset($_SESSION['NISN_SISWA'])) {
     header("Location: ../index.php");
     exit;
 }
+
 $nisn = $_SESSION['NISN_SISWA'];
 global $connect;
 
@@ -12,7 +13,9 @@ $stmnt = $connect->prepare("SELECT * FROM siswa WHERE NISN_SISWA = :nisn");
 $stmnt->execute([':nisn' => $nisn]);
 $siswa = $stmnt->fetch();
 ?>
-
+<?php
+include "../components/header.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,17 +23,16 @@ $siswa = $stmnt->fetch();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../source/css/style.css">
 </head>
 
 <body>
-    selamat datang di web kami
-    <img src="../source/upload/images/<?= $siswa['FOTO_SISWA_SISWA']; ?>" alt="Foto Siswa" width="100"
-        style="border-radius: 50%;">
-    <p><?= htmlspecialchars($siswa['NAMA_LENGKAP_SISWA']); ?></p>
-    </div>
-
-    <a href="../siswa/edit_siswa.php">Edit Profil</a> |
-    <a href="../auth/logout.php">Logout</a>
+    <section class="header">
+        <div class="container">
+            <h2>Selamat Datang di PPDB Online</h2>
+            <p>Sekolah Berbasis Pesantren Modern, Mencetak Generasi Qur'ani dan Berprestasi.</p>
+        </div>
+    </section>
 
 </body>
 
